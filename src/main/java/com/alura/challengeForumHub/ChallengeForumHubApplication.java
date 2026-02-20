3717c7c2740a943333e3a931d34d444039124373
Patricia <patriciahm7@hotmail.com>
2026-02-20 16:48:06 -0300

    first commit

---
+++ src/main/java/com/alura/challengeForumHub/ChallengeForumHubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challengeForumHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForumHubApplication.class, args);
+	}
+
+}
